8070cbc7ee139766d39845a70fc63ce52f185161
Mojens <moha135h@stud.kea.dk>
2021-12-05 00:39:53 +0100

    Tilføjet List sammen med ArrayList

--- src/Comparator/Runner.java
+++ src/Comparator/Runner.java
@@ -7,12 +7,21 @@ import java.util.List;
 public class Runner {
   public static void main(String[] args) {
 
-    List<Laptop> laps = new ArrayList<>();
+    ArrayList<Laptop> lapsArrayList = new ArrayList<>(); //ArrayList er en class
+//Her laver vi 3 forskellige objekter og tilføjer dem til ArrayListen
+
+    lapsArrayList.add(new Laptop("Dell",16,800));
+    lapsArrayList.add(new Laptop("Apple",8,2000));
+    lapsArrayList.add(new Laptop("Acer",12,700));
+
+    List<Laptop> lapsList = new ArrayList<>(); //List er en interface
 //Her laver vi 3 forskellige objekter og tilføjer dem til Listen
 
-    laps.add(new Laptop("Dell",16,800));
-    laps.add(new Laptop("Apple",8,2000));
-    laps.add(new Laptop("Acer",12,700));
+    lapsList.add(new Laptop("Dell",25,2000));
+    lapsList.add(new Laptop("Apple",3,1100));
+    lapsList.add(new Laptop("Acer",11,400));
+
+
 
     //Her laver vi en Comparator med Datatypen Laptop, hvor den skal sorter ud fra pris.
 
@@ -25,16 +34,6 @@ public class Runner {
       }
     };
 
-    Collections.sort(laps, sortPrice); // Her bruger vi Collections.sort, hvor vi først angive listen, og derefter angiver den måde den skal sorteres på
-
-    System.out.println("Printer Sortering med pris Lav til høj\n");
-    for (Laptop l : laps) {
-      System.out.println(l);
-    }
-    System.out.println("\n");
-
-
-
     Comparator<Laptop> sortRam = new Comparator<Laptop>(){ // Her Laver vi en Comparator som vi kalder for sortRam
       public int compare(Laptop l1, Laptop l2){ // Vi laver en compare metode med to objekter som paramtre af den samme datatype
        if (l1.getRam() < l2.getRam()) //Her sammenligner vi
@@ -43,11 +42,37 @@ public class Runner {
          return -1;
       }
     };
-    Collections.sort(laps, sortRam);// Her bruger vi Collections.sort, hvor vi først angive listen, og derefter angiver den måde den skal sorteres på
-    // Hvor i dette tilfælde skal den ikke sorter efter pris men sorter efter ram størrelse.
-    System.out.println("Printer sortering med Ram Høj til Lav\n");
-    for (Laptop l : laps) {
+
+    System.out.println("\n");
+
+    Collections.sort(lapsArrayList, sortPrice); // Her bruger vi Collections.sort, hvor vi først angive ArrayListen, og derefter angiver den måde den skal sorteres på
+    System.out.println("Printer Sortering af ArrayList med pris Lav til høj\n");
+    for (Laptop l : lapsArrayList) {
       System.out.println(l);
     }
+    System.out.println("\n");
+
+    Collections.sort(lapsArrayList, sortRam);// Her bruger vi Collections.sort, hvor vi først angive ArrayListen, og derefter angiver den måde den skal sorteres på
+    // Hvor i dette tilfælde skal den ikke sorter efter pris men sorter efter ram størrelse.
+    System.out.println("Printer sortering af ArrayList med Ram Høj til Lav\n");
+    for (Laptop l : lapsArrayList) {
+      System.out.println(l);
+    }
+    System.out.println("\n");
+
+    Collections.sort(lapsList, sortPrice); // Her bruger vi Collections.sort, hvor vi først angive Listen, og derefter angiver den måde den skal sorteres på
+    System.out.println("Printer Sortering af Listen med pris Lav til høj\n");
+    for (Laptop l : lapsList) {
+      System.out.println(l);
+    }
+    System.out.println("\n");
+
+    Collections.sort(lapsList, sortRam);// Her bruger vi Collections.sort, hvor vi først angive Listen, og derefter angiver den måde den skal sorteres på
+    // Hvor i dette tilfælde skal den ikke sorter efter pris men sorter efter ram størrelse.
+    System.out.println("Printer sortering af Listen med Ram Høj til Lav\n");
+    for (Laptop l : lapsList) {
+      System.out.println(l);
+    }
+
   }
 }
